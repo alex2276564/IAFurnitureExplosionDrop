8ffc8248d3704f38971dc07ae7d8da8cb23bb69a
alex2276564 <you@example.com>
2025-02-06 22:44:40 +0300

    fix build 3

--- src/main/java/uz/alex2276564/iafurnitureexplosiondrop/IAFurnitureExplosionDrop.java
+++ src/main/java/uz/alex2276564/iafurnitureexplosiondrop/IAFurnitureExplosionDrop.java
@@ -1,17 +1,40 @@
 package uz.alex2276564.iafurnitureexplosiondrop;
 
+import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import uz.alex2276564.iafurnitureexplosiondrop.listeners.IAFurnitureExplosionListener;
+import uz.alex2276564.iafurnitureexplosiondrop.task.BukkitRunner;
+import uz.alex2276564.iafurnitureexplosiondrop.task.Runner;
+import uz.alex2276564.iafurnitureexplosiondrop.utils.UpdateChecker;
 
 public final class IAFurnitureExplosionDrop extends JavaPlugin {
 
+    @Getter
+    private Runner runner;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        setupRunner();
+        registerListeners();
+        checkUpdates();
+    }
 
+    private void setupRunner() {
+        runner = new BukkitRunner(this);
+    }
+
+    private void registerListeners() {
+        Bukkit.getPluginManager().registerEvents(new IAFurnitureExplosionListener(), this);
+    }
+
+    private void checkUpdates() {
+        UpdateChecker updateChecker = new UpdateChecker(this, "alex2276564/IAFurnitureExplosionDrop", runner);
+        updateChecker.checkForUpdates();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        runner.cancelTasks();
     }
 }
