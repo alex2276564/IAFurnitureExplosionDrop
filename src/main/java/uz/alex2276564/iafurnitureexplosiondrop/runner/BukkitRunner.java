92b790b6d4a4d1b959fd9aa5859b681cd11b1eac
alex2276564 <you@example.com>
2025-06-06 00:53:38 +0300

    refactor

--- src/main/java/uz/alex2276564/iafurnitureexplosiondrop/runner/BukkitRunner.java
+++ src/main/java/uz/alex2276564/iafurnitureexplosiondrop/runner/BukkitRunner.java
@@ -1,4 +1,4 @@
-package uz.alex2276564.iafurnitureexplosiondrop.task;
+package uz.alex2276564.iafurnitureexplosiondrop.runner;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
